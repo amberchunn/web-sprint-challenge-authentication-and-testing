a099f308e286b1b168a3ff375947badb76d169d7
Amber Chunn <a.77@me.com>
2020-07-17 22:34:44 -0700

    Added Routes, Models, and Auth Functionality

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,64 @@
+const express = require("express");
+const bcrypt = require('bcryptjs');
+const jwt = require("jsonwebtoken");
+const db = require("../database/dbConfig");
+const jokesters = require("./auth-model");
+const restricted = require('./authenticate-middleware/restricted');
+
 const router = require('express').Router();
 
-router.post('/register', (req, res) => {
-  // implement registration
-});
+const errorMsg = "Non!"
 
-router.post('/login', (req, res) => {
-  // implement login
-});
+router.post("/register", async (req, res, next) => {
+  // implement registration
+
+	try {
+		const { username } = req.body;
+		const user = await jokesters.findBy({username});
+
+		if (user) {
+			return res.status(409).json({
+				message: "Username is already taken",
+			})
+		}
+
+		res.status(201).json(await jokesters.add(req.body));
+	} catch(err) {
+		next(err);
+	}
+})
+
+router.post('/login', restricted, async (req, res, next) => {
+      // implement login
+
+    try {
+      const { username, password } = req.body;
+      const joker = await jokesters.findBy({username });
+
+      if(!joker) {
+        return res.status(401).json(errorMsg);
+      }
+
+      const validated = await bcrypt.compare({password}, joker.password);
+
+      const session = await jokesters.add({
+        user_id: user.id,
+        expires: 0
+      });
+
+       const token = jwt.sign({
+            sessionId: session.id,
+            username: joker.username,
+          }, process.env.JWT_SECRET );
+
+      res.cookie("token", token);
+
+      res.json({message: `${joker.username}, you are now authenticated`,})
+
+    } catch(err) {
+        next(err);
+      }
+
+  });
 
 module.exports = router;
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,13 @@
-/* 
+const express = require('express');
+
+/*
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
 
-module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
-};
+const restricted  = (req, res, next) => {
+  return res.status(401).json({ you: 'shall not pass!' });
+  next();
+}
+
+module.exports = restricted;
